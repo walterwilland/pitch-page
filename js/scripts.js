216a8a7cb5e76cb51f081df3e8b567ce5dc35aee
Walter Willand <wwilland@engr-131-18.eastern.ewu.edu>
2016-05-16 16:41:14 -0700

    initial

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// First, load jQuery (required)
+// jQuery 2.2.2 loaded via `index.html`
+
+// Second, load Semantic UI JS (required)
+// @codekit-prepend "semantic.js";
+
+// Third, load any plugins (optional)
+// @codekit-prepend "plugins.js";
+
+// My Scripts
